7aade88c94f3b459b820d9d242de494e6529b2d0
Robert Basamac <rbasamac@gmail.com>
2022-08-13 12:48:45 +0300

    Initial Commit

---
+++ CryptoTracker/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CryptoTracker
+//
+//  Created by Robert Basamac on 13.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Robert Basamac on 13.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
